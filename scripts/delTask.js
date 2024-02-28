8d715cbd41d81c4c38b3f8b908c72a3eb989a4b3
yeeun3127 <114934854+yeeun3127@users.noreply.github.com>
2024-02-29 02:32:46 +0900

    first commit

--- scripts/delTask.js
+++ scripts/delTask.js
@@ -1,4 +1,12 @@
 /* 할 일 삭제 함수 */
-const delTask = (event) => {
-    console.log("할 일 삭제 완료")
+function deleteTask(id) {
+  console.log("deleteTask 함수 실행", id);
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList.splice(i, 1);
+      break;
+    }
+    console.log(taskList);
+  }
+  render();
 }
--- scripts/render.js
+++ scripts/render.js
@@ -1,28 +1,29 @@
 let resultHTML; //화면에 그려줄 HTML
 const render = () => {
-    console.log("render 함수 실행");
-    
-    /*로컬스토리지에 이전 값들이 저장되어있는지 확인하기 (저장안됐으면 null)
-    key값을 이용하여 로컬스토리지에서 할 일 목록 리스트를 가져온다.*/
-    const isSavedTodo = localStorage.getItem(TODO_KEY);
-    if (isSavedTodo) { //값이 있다면
-        console.log("값이 있습니다.");
-        /*isSavedTodo가 null이 아니라면 이전에 저장한 기록이 있으므로
-          이전에 저장한 값을 string -> object (array) 형태로 변경*/
-        taskList = JSON.parse(localStorage.getItem(TODO_KEY));
-        console.log("값o",taskList);
-    }
-    else { //값이 없다면 처음 사용하거나 새로고침이 된 것이므로 taskList를 빈리스트로
-        console.log("값이 없습니다.")
-        taskList = []
-        console.log("값x",taskList)
-    }
+  console.log("render 함수 실행");
 
-    resultHTML = ``;
-    
-    //로컬스토리지에 저장된 할 일 리스트를 순회한다.
-    for(let i=0; i<taskList.length; i++){
-        resultHTML += `
+  /*로컬스토리지에 이전 값들이 저장되어있는지 확인하기 (저장안됐으면 null)
+    key값을 이용하여 로컬스토리지에서 할 일 목록 리스트를 가져온다.*/
+  const isSavedTodo = localStorage.getItem(TODO_KEY);
+  if (isSavedTodo) {
+    //값이 있다면
+    console.log("값이 있습니다.");
+    /*isSavedTodo가 null이 아니라면 이전에 저장한 기록이 있으므로
+          이전에 저장한 값을 string -> object (array) 형태로 변경*/
+    taskList = JSON.parse(localStorage.getItem(TODO_KEY));
+    console.log("값o", taskList);
+  } else {
+    //값이 없다면 처음 사용하거나 새로고침이 된 것이므로 taskList를 빈리스트로
+    console.log("값이 없습니다.");
+    taskList = [];
+    console.log("값x", taskList);
+  }
+
+  resultHTML = ``;
+
+  //로컬스토리지에 저장된 할 일 리스트를 순회한다.
+  for (let i = 0; i < taskList.length; i++) {
+    resultHTML += `
             <div class="task">
             <div class = "wrap1">
                 <button id = "task-done-btn" onclick= "completeTask()"></button>
@@ -30,11 +31,11 @@ const render = () => {
             </div>
             <div class = "wrap2">
                 <button id = "task-edit-btn" onclick = "editTask()"></button>
-                <button id = "task-delete-btn" onclick = "delTask()"></button>
+                <button id = "task-delete-btn" onclick = "deleteTask('${taskList[i].id}')"></button>
             </div>
         </div>
-        `
-    }
-    document.getElementById("task-area-board").innerHTML = resultHTML;
-}
+        `;
+  }
+  document.getElementById("task-area-board").innerHTML = resultHTML;
+};
 render();
