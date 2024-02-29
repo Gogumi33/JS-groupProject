bff01854e57394b1e046fdaf367cf44b4ef5401a
ceunnseo <eunseo4068@gmail.com>
2024-02-29 15:32:51 +0900

    completeTask 함수 작성

--- scripts/completeTask.js
+++ scripts/completeTask.js
@@ -1,4 +1,5 @@
 const checkButton = document.getElementById("task-done-btn");
+let renderStatus = "doing";
 
 /* 할 일 체크 함수 : taskList의 isComplete를 바꿔주고 다시 로컬스토리지에 저장하여 렌더 */
 function completeTask(taskId) {
@@ -8,11 +9,12 @@ function completeTask(taskId) {
     for (let i = 0; i < taskList.length; i++) {
         if (taskList[i].id === taskId) {
             taskList[i].isComplete = !taskList[i].isComplete;
+            renderStatus = taskList[i].isComplete ? "doing" : "done";
             break;
         }
     }
     localStorage.setItem(TODO_KEY, JSON.stringify(taskList));
     console.log(taskList);
     console.log("끝");
-    render();
+    render(renderStatus);
 }
--- scripts/render.js
+++ scripts/render.js
@@ -1,11 +1,13 @@
 let resultHTML; //화면에 그려줄 HTML
-const render = () => {
+let filterlist = []; //로컬스토리지에서 필터링 된 값들
+const render = (mode) => {
     console.log("render 함수 실행");
     
     /*로컬스토리지에 이전 값들이 저장되어있는지 확인하기 (저장안됐으면 null)
     key값을 이용하여 로컬스토리지에서 할 일 목록 리스트를 가져온다.*/
     const isSavedTodo = localStorage.getItem(TODO_KEY);
-    if (isSavedTodo) { //값이 있다면
+
+    if (isSavedTodo) { //로컬스토리지에 값이 있다면
         console.log("값이 있습니다.");
         /*isSavedTodo가 null이 아니라면 이전에 저장한 기록이 있으므로
           이전에 저장한 값을 string -> object (array) 형태로 변경*/
@@ -21,37 +23,44 @@ const render = () => {
     resultHTML = ``;
     
     //로컬스토리지에 저장된 할 일 리스트를 순회한다.
-    for(let i=0; i<taskList.length; i++){
-        if (taskList[i].isComplete) { //다 끝낸 일이라면
-            resultHTML += `
-            <div id = ${taskList[i].id} class="task">
-                <div class = "wrap1">
-                    <button id = "task-done-btn" onclick= "completeTask('${taskList[i].id}')"></button>
-                    <div class = "task-done-name">${taskList[i].taskContent}</div>
+    if (mode === "doing") { //doing이라면 isComplete가 false인 일들을 리턴해준다.
+        for(let i=0; i<taskList.length; i++){
+            if (!taskList[i].isComplete) { //아직 진행중이라면 (false)
+                resultHTML += `
+                <div id = ${taskList[i].id} class="task">
+                    <div class = "wrap1">
+                        <button id = "task-done-btn" onclick= "completeTask('${taskList[i].id}')"></button>
+                        <div class = "task-doing-name">${taskList[i].taskContent}</div>
+                    </div>
+                    <div class = "wrap2">
+                        <button id = "task-edit-btn" onclick = "editTask('${taskList[i].id}')"></button>
+                        <button id = "task-delete-btn" onclick = "delTask('${taskList[i].id}')"></button>
+                    </div>
                 </div>
-                <div class = "wrap2">
-                    <button id = "task-edit-btn" onclick = "editTask('${taskList[i].id}')"></button>
-                    <button id = "task-delete-btn" onclick = "delTask('${taskList[i].id}')"></button>
-                </div>
-            </div>
-        `
+            `
+            }
         }
-        else { //다 끝내지 못한 일이라면(진행중)
-            resultHTML += `
-            <div id = ${taskList[i].id} class="task">
-                <div class = "wrap1">
-                    <button id = "task-done-btn" onclick= "completeTask('${taskList[i].id}')"></button>
-                    <div class = "task-doing-name">${taskList[i].taskContent}</div>
+    }
+    else { //done이라면 isComplete가 true인 일들을 리턴해준다.
+        for(let i = 0; i < taskList.length; i++) {
+            if (taskList[i].isComplete) { //다 끝낸 일이라면
+                resultHTML += `
+                <div id = ${taskList[i].id} class="task">
+                    <div class = "wrap1">
+                        <button id = "task-done-btn" onclick= "completeTask('${taskList[i].id}')"></button>
+                        <div class = "task-doing-name">${taskList[i].taskContent}</div>
+                    </div>
+                    <div class = "wrap2">
+                        <button id = "task-edit-btn" onclick = "editTask('${taskList[i].id}')"></button>
+                        <button id = "task-delete-btn" onclick = "delTask('${taskList[i].id}')"></button>
+                    </div>
                 </div>
-                <div class = "wrap2">
-                    <button id = "task-edit-btn" onclick = "editTask('${taskList[i].id}')"></button>
-                    <button id = "task-delete-btn" onclick = "delTask('${taskList[i].id}')"></button>
-                </div>
-            </div>
-        `
+            `
+            }
         }
 
     }
+    
     document.getElementById("task-area-board").innerHTML = resultHTML;
 }
-render();
+render("doing");
