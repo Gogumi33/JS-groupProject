e0ea65e0c684ebc919e37c197e8982f4fc7b435c
ceunnseo <eunseo4068@gmail.com>
2024-02-29 23:03:44 +0900

    코드 정리

--- scripts/completeTask.js
+++ scripts/completeTask.js
@@ -1,20 +1,18 @@
 const checkButton = document.getElementById("task-done-btn");
-let renderStatus = "doing";
+let renderStatus = "doing"; //어떤 화면을 render할 지 상태를 저장하는 변수
 
-/* 할 일 체크 함수 : taskList의 isComplete를 바꿔주고 다시 로컬스토리지에 저장하여 렌더 */
+/* 체크를 통해 진행중 <-> 진행완료 상태를 바꿔주는 completeTask 함수
+   체크버튼을 클릭할 때 실행되며 onclick으로 함수 실행
+   - taskList의 isComplete를 바꿔주고 다시 로컬스토리지에 저장하여 렌더 */
 function completeTask(taskId) {
-    console.log("체크 누름");
-    taskList = JSON.parse(localStorage.getItem(TODO_KEY));
-    console.log(taskList);
-    for (let i = 0; i < taskList.length; i++) {
-        if (taskList[i].id === taskId) {
-            taskList[i].isComplete = !taskList[i].isComplete;
-            renderStatus = taskList[i].isComplete ? "doing" : "done";
-            break;
+    taskList = JSON.parse(localStorage.getItem(TODO_KEY)); //로컬스토리지에서 할 일을 가져오고, object로 바꿔서 taskList에 저장
+    for (let i = 0; i < taskList.length; i++) { //taskList에서 해야할 값들을 탐색하며
+        if (taskList[i].id === taskId) { //내가 클릭한 해야할 일을 찾았으면
+            taskList[i].isComplete = !taskList[i].isComplete; //complete 여부를 뒤집고 
+            renderStatus = taskList[i].isComplete ? "doing" : "done"; //뒤집은 창이 화면에 나와야하므로 renderState값도 변경
+            break; //더이상 탐색이 필요 없으니 종료
         }
     }
-    localStorage.setItem(TODO_KEY, JSON.stringify(taskList));
-    console.log(taskList);
-    console.log("끝");
-    render(renderStatus);
+    localStorage.setItem(TODO_KEY, JSON.stringify(taskList)); //다시 해야 할 일을 string형태로 바꿔서 로컬스토리지에 저장하고
+    render(renderStatus); //화면에 출력
 }
--- scripts/delTask.js
+++ scripts/delTask.js
@@ -1,8 +1,9 @@
-/* 할 일 삭제 함수 */
-function deleteTask(id) {
-    console.log("deleteTask 함수 실행", id);
+/* 할 일 삭제 함수
+휴지통 버튼을 누르면 해당 id값을 가진 (taskId) 할 일이 삭제됨
+*/
+function deleteTask(taskId) {
     for (let i = 0; i < taskList.length; i++) {
-      if (taskList[i].id === id) {
+      if (taskList[i].taskId === taskId) {
         //삭제하고자 하는 데이터의 isComplete이 true이면 다 함 (done), false이면 하는중 (doing)
         renderStatus = taskList[i].isComplete ? "done" : "doing"; 
         taskList.splice(i, 1);
