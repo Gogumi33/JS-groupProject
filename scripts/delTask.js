f7a9f0d05d63fa8dcb5a4edc2ffd1e4e999967ce
yeeun3127 <114934854+yeeun3127@users.noreply.github.com>
2024-02-29 20:53:01 +0900

    deteteTask + localstorage 함수수정

--- scripts/delTask.js
+++ scripts/delTask.js
@@ -1,12 +1,24 @@
 /* 할 일 삭제 함수 */
+// function deleteTask(id) {
+//   console.log("deleteTask 함수 실행", id);
+//   for (let i = 0; i < taskList.length; i++) {
+//     if (taskList[i].id === id) {
+//       taskList.splice(i, 1);
+//       break;
+//     }
+//     console.log(taskList);
+//   }
+//   render();
+// }
+
 function deleteTask(id) {
   console.log("deleteTask 함수 실행", id);
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id === id) {
       taskList.splice(i, 1);
+      localStorage.setItem(TODO_KEY, JSON.stringify(taskList)); // 로컬 스토리지에서 해당 데이터 삭제
       break;
     }
-    console.log(taskList);
   }
   render();
 }
