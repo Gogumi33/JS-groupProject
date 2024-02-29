565b1af01590092d93505b3c0cd5153a2b054c66
yeeun3127 <114934854+yeeun3127@users.noreply.github.com>
2024-02-29 23:16:03 +0900

    주석 제거

--- scripts/delTask.js
+++ scripts/delTask.js
@@ -1,15 +1,4 @@
 /* 할 일 삭제 함수 */
-// function deleteTask(id) {
-//   console.log("deleteTask 함수 실행", id);
-//   for (let i = 0; i < taskList.length; i++) {
-//     if (taskList[i].id === id) {
-//       taskList.splice(i, 1);
-//       break;
-//     }
-//     console.log(taskList);
-//   }
-//   render();
-// }
 
 function deleteTask(id) {
   console.log("deleteTask 함수 실행", id);
