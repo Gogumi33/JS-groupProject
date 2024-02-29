faacb23e7439e8c9d29b3ce51cc6c4672405dddb
ceunnseo <eunseo4068@gmail.com>
2024-03-01 00:20:57 +0900

    삭제함수 수정

--- scripts/delTask.js
+++ scripts/delTask.js
@@ -2,8 +2,9 @@
 휴지통 버튼을 누르면 해당 id값을 가진 (taskId) 할 일이 삭제됨
 */
 function deleteTask(taskId) {
+  console.log("삭제 함수 실행")
     for (let i = 0; i < taskList.length; i++) {
-      if (taskList[i].taskId === taskId) {
+      if (taskList[i].id === taskId) {
         //삭제하고자 하는 데이터의 isComplete이 true이면 다 함 (done), false이면 하는중 (doing)
         renderStatus = taskList[i].isComplete ? "done" : "doing"; 
         taskList.splice(i, 1);
