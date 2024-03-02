a4323a2cd2147c8a4dc406fd75dea8f808bba587
banya <bjgeumgang@gmail.com>
2024-03-02 16:19:51 +0900

    머지 완료후 변경1

--- scripts/deadLine.js
+++ scripts/deadLine.js
@@ -1,9 +1,9 @@
-let timePicker = document.getElementById("time-picker"); // 타임피커 input창 가져오기
+let DateTimePicker = document.getElementById("date-time-picker"); // 데이트타임피커 input창 가져오기
 let deadWrap = document.getElementById("dead"); // taskList에 담긴 데드라인 (render.js에 위치)
 const btnClose = document.getElementById("btnClose"); // X(창닫는 버튼)가져오기
 
 /* X버튼 클릭시 타임피커 초기화 */
 const resetTimePicker = () => {
-  timePicker.value = "";
+  DateTimePicker.value = "";
 };
 btnClose.addEventListener("click", resetTimePicker);
