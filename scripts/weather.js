34cde5260ecbb97a31030ae1b7e1ee5ddc191b61
ceunnseo <eunseo4068@gmail.com>
2024-03-03 01:39:32 +0900

    위치와 날씨 정보 fetch

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,10 +1,13 @@
 const weather = document.getElementById("current-weather");
 const API_KEY = `073de8edeeb6cfc808889bd776923a93`;
 
-// 1. 사용자 위치 정보 가져오기 (위도, 경도)
-window.navigator.geolocation.getCurrentPosition(function(position) {
-    let lat = position.coords.latitude;
-    let lng = position.coords.longitude;
-
-    console.log("위도경도",lat, lng);
+navigator.geolocation.getCurrentPosition(async function(pos) {
+    let lat = pos.coords.latitude;
+    let lon = pos.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url)
+    const data = await response.json();
+    console.log("data!!!", data);
+    console.log(data.weather[0].icon);
 })
+
