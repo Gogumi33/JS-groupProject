65f5bcb9c7babc8900759c1c663b4d9f4dc5d741
ceunnseo <eunseo4068@gmail.com>
2024-03-03 16:07:12 +0900

    마이크 on/off hover 적용&마이크 꺼지도록

--- scripts/recordOnBtn.js
+++ scripts/recordOnBtn.js
@@ -1,14 +1,17 @@
 const micAddOnBtn = document.getElementById("micAddOnBtn");
 let editMicTaskDead;
 let editMicTaskId;
+let micState = "off"
 
 const recordOnAddBtn = () => {
   availabilityFunc();
   recognition.addEventListener("speechstart", () => {});
-
+  
+  
   //음성인식이 끝까지 이루어지면 중단된다.
   recognition.addEventListener("speechend", () => {});
 
+  //
   //음성인식 결과를 반환
   // SpeechRecognitionResult 에 담겨서 반환된다.
   recognition.addEventListener("result", (e) => {
@@ -19,15 +22,10 @@ const recordOnAddBtn = () => {
       recordStop();
       const editMicTask = e.results[0][0].transcript.split(' ')[0];
       const micEditButton = document.getElementById(`${editMicTask}`)
-      console.log(editMicTask);
       
-      console.log("!!!!!!!!!!!!!!!!!!!!!!!",micEditButton, editMicTaskDead, editMicTaskId);
       micEditButton.addEventListener("click", async (event) => {
         for (let i = 0; i < taskList.length; i++) {
           if (taskList[i].taskContent == editMicTask) {
-            // 수정해야할 task의 id찾기
-            //수정하고자 하는 데이터의 isComplete이 true이면 진행완료 (done), false이면 진행중 (doing)
-            renderStatus = taskList[i].isComplete ? "done" : "doing";
             editMicTaskDead = await taskList[i].deadlineTime
             editMicTaskId = taskList[i].id
             break
@@ -51,13 +49,25 @@ const recordOnAddBtn = () => {
         deleteTask(resultString);
       
     } 
-
+    micAddOnBtn.style.backgroundImage = "url('./img/micOff.svg')"
     console.log(e.results[0][0].transcript);
   });
 
   recognition.start(); //음성인식을 시작
+  
 };
 
 micAddOnBtn.addEventListener("click", () => {
-  recordOnAddBtn();
+  if (micState === "off") { //마이크가 꺼져있을 때 클릭하면 켜진다.
+    micState = "on"
+    console.log(micState)
+    micAddOnBtn.style.backgroundImage = "url('./img/micStart.svg')"
+    recordOnAddBtn();
+  }
+  else { //마이크가 켜져있을 때 클릭하면 꺼진다.
+    micState = "off"
+    micAddOnBtn.style.backgroundImage = "url('./img/micOff.svg')"
+    recordStop();
+  }
+
 });
